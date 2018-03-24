389819ae353bfe2bfd39ac08e4c59b542105e509
Yen H <yen@172-30-35-250.DYNAPOOL.NYU.EDU>
2018-03-24 15:22:34 -0400

    correction to home page

--- cms/home.js
+++ cms/home.js
@@ -1,14 +1,15 @@
 {
-  brand:{
+  brand: {
+    $link_type: 'image',
     link: 'https://www.kelloggsspecialtychannels.com/Image/Show/5e03132c-e27c-4a54-9e25-0f970a8e7eac',
 		tagline1: 'Be the Hero this world deserves but not the one it needs right now.',
     tagline2: 'About Us: A web app, in which we help the world.'
-  }
-  mission:{
+  },
+  mission: {
     header: 'Mission',
-      tagline1: 'Everybody in the world plays a part in other people\'s lives. It is important to create a health environment where people look after each other. In this case we present BA.Heroes; an application where people "cry for help" and "Heroes to Rescue".',
-      tagline2: 'How it works',
-
+    $tagline1_type: 'textarea',
+    tagline1: 'Everybody in the world plays a part in other people\'s lives. It is important to create a health environment where people look after each other. In this case we present BA.Heroes; an application where people "cry for help" and "Heroes to Rescue".',
+    tagline2: 'How it works'
   }
 
 }
