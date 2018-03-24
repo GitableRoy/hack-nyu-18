9099eb9c2e37d547ef240d49c7d2dbd48393d74c
Amber Ye <Amber@172-30-34-54.DYNAPOOL.NYU.EDU>
2018-03-24 19:55:19 -0400

    updated home page

--- cms/layout/home.js
+++ cms/layout/home.js
@@ -1,3 +1,29 @@
 {
-  title: 'Welcome to our site'
+  brand: {
+    $link_type: 'image',
+    link: 'https://cdn.pixabay.com/photo/2016/02/09/05/27/smoke-1188334_960_720.jpg',
+    title: 'BA.Hero',
+		tagline1: 'Be the Hero this world deserves but not the one it needs right now.',
+    tagline2: 'About Us: A web app, in which we help the world.'
+
+  },
+  mission: {
+    header: 'Mission',
+    $tagline1_type: 'textarea',
+    tagline1: 'Everybody in the world plays a part in other people\'s lives. It is important to create a health environment where people look after each other. In this case we present BA.Heroes; an application where people "cry for help" and "Heroes to Rescue".',
+    tagline2: 'How it works'
+  },
+  explanation:{
+    header: 'How it works',
+    step1: 'Sign up!',
+    step2: 'Search the board!',
+    step3:'Earn points'
+  },
+
+  join:{
+    header:'how?',
+  }
+
+
+
 }
